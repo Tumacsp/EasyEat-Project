3a9694f15ea2c8d1a2e6af1bd0d334349dd16168
Tum_118 <105029651+Tumacsp@users.noreply.github.com>
2025-04-17 11:21:17 +0700

    Update connectDB.php

--- backEnd/includes/connectDB.php
+++ backEnd/includes/connectDB.php
@@ -1,19 +1,15 @@
 <?php
 
-    $db_host = "161.246.127.24";
-    $db_port = 9041;
-    $db_user = "clswsxdz60005bsmnhc5pc95b";
-    $db_password = "yFvP9V42vLTdP4vlOufQY1OA";
+    $db_host = "xxx";
+    $db_port = 1234;
+    $db_user = "xxx";
+    $db_password = "xxx";
     $db_name = "Easyeat";
 
-    // Create connection
     try {
-        // เชื่อมต่อกับ MySQL
         $db = new PDO("mysql:host=$db_host;port=$db_port;dbname=$db_name", $db_user, $db_password);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        //echo "Success!";
     } catch (PDOException $e) {
         echo "Failed to connect" . $e->getMessage();
     }
-
 ?>
